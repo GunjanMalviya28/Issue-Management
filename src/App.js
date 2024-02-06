75184d61c582a39436eb30a05be605c6d8807994
Gunjan Malviya <GMalviya@bpl.netlink.com>
2024-02-06 18:10:11 +0530

    adding channel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
 import Table from './Table';
+import To_dolist from './To_dolist.js';
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <To_dolist/>
     <Table/>
 
     </div>
--- src/To_dolist.js
+++ src/To_dolist.js
@@ -1,14 +1,22 @@
+"use client"
 import './App.css';
-import React from 'react'
+import React, { useState } from "react";
 
 const To_dolist = () => {
+ const [title,settitle] = useState("")
+ const [desc,setdesc] = useState("")
   return (
     <>
       <form>
-        <input type="text" />
+      <input type="text" className="text-2xl border-zinc-800 border-4 m-8 px-4 py-2 " placeholder="Enter Issue here" 
+      value={title} onChange={(e) => {settitle(e.target.value)}}/>
+     
+     <input type="text" className="text-2xl border-zinc-800 border-4 m-18 px-4 py-2" placeholder="Enter Description here" />
+      
+      <button className='bg-black text-white px-4 py-3 text-2xl font-bold rounded m-5'>Add Issue</button>
       </form>
     </>
-  )
+  );
 }
 
 export default To_dolist
