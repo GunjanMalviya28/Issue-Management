8be932dc2e5116c12b9a65cfcbe5cb32f78ec443
Gunjan Malviya <GMalviya@bpl.netlink.com>
2024-02-06 12:31:50 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import Table from './Table';
+import login_signup from './login_signup';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+    <Table/>
+    <login_signup/>
     </div>
   );
 }
